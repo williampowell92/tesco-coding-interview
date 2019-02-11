386cd223b27b1680968efc858f16a5ee589db50e
William Powell <william.powell92@gmail.com>
2019-02-11 20:30:13 +0000

    Hello world

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HelloWorldTest {
+    private HelloWorld helloWorld;
+
+    @Before
+    public void setup() {
+        helloWorld = new HelloWorld();
+    }
+
+    @Test
+    public void helloWorld() {
+        assertEquals("Hello, world!", helloWorld.helloWorld());
+    }
+}
